b677d710eac4d2b033351a99035315c5d417e6a1
RZhang <richardzhiyuanzhang@gmail.com>
2022-07-27 21:55:29 -0400

    updated User.js, resolvers and typedefs. Created three basic user seeds.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,11 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    firstName: String
-    lastName: String
+    username: String
     email: String
+    wins: Int
+    ties: Int
+    losses: Int
   }
 
   type Auth {
@@ -18,8 +20,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    addUser(username: String!, email: String!, password: String!): Auth
+    updateUser(username: String!, email: String!, password: String!): User
     login(email: String!, password: String!): Auth
   }
 `;
