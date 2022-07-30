f9e00037b5f6c9663274828f549a469679871592
RZhang <richardzhiyuanzhang@gmail.com>
2022-07-30 14:09:34 -0400

    updated chat logic and added preliminary styling with conditional styles

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { SocketContext } from '../../utils/socket'
+import './Chat.css'
 
 export default function Chat(props) {
     const user = props.user.user;
+    const room = props.room;
+    const date = new Date();
+    const time = date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
     const [input, setInput] = useState({
         user: user.username,
+        time: time,
         message: ''
     });
     const [messages, setMessages] = useState([]);
-    const room = props.room;
-
     const socket = useContext(SocketContext);
 
-    const sendMessage = () => {
+    const sendMessage = (e) => {
+        e.preventDefault();
         if (input) {
             setMessages((prevMessages) => [input, ...prevMessages]);
             socket.emit('sendMessage', { message: input, room: room })
         }
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                message: ''
+            }
+        })
     };
 
     const handleMessage = (e) => {
         const message = e.target.value;
         setInput(prevMessage => {
-            return { ...prevMessage, message: message}
+            return { ...prevMessage, message: message, time: time}
         });
     };
 
@@ -33,33 +46,58 @@ export default function Chat(props) {
     }, [socket]);
 
     return (
-        <section style={{ display: 'flex-column'}}>
-            <div className='chat' style={{ height: '400px', width: '300px', overflow: 'auto', border: 'gray solid 1px'}}>
-                <div style={{ display: 'flex-column', alignItems: 'start', justifyContent: 'end' }}>
+        <section className='chat'>
+            <div className='chat--window'>
+                <div className='chat--messages--box'>
                     {messages.map((message, i) => {
+                        const isUser = message.user === user.username
                         return (
-                            <div key={i}>
-                                <p style={{ marginBottom: '0', paddingBottom: '0' }}>{message.message}</p>
-                                <p style={{ marginBottom: '0', paddingBottom: '0' }}>
-                                    <small>{message.user == user.username ? 'You' : message.user}</small>
+                            <div 
+                                className='chat--message--container'
+                                style={{ 
+                                    alignSelf: `${isUser ? 'start' : 'end'}`,
+                                    color: `${isUser ? 'black' : 'white'}`,
+                                    backgroundColor: `${isUser ? 'skyblue' : 'rgb(5, 139, 5)'}`
+                                }} 
+                                key={i}
+                            >
+                                <p 
+                                    className='chat--message' 
+                                    style={{
+                                        alignSelf: `${isUser ? 'start' : 'end'}`, 
+                                    }}
+                                >
+                                    {message.message}
+                                </p>
+                                <p 
+                                    className='chat--message--user' 
+                                    style={{
+                                        alignSelf: `${isUser ? 'start' : 'end'}`, 
+                                    }}
+                                >
+                                    {isUser ? 'You' : message.user}, {message.time}
                                 </p>
                             </div>
                         )
                     })}
                 </div>
             </div>
-            <input 
-                placeholder='message'
-                name='message'
-                type='text'
-                id='message'
-                onChange={handleMessage}
-            />
-            <button
-                onClick={sendMessage}
-            >
-                Send
-            </button>
+            <form className='chat--form' onSubmit={sendMessage}>
+                <input
+                    className='chat--input' 
+                    placeholder='message'
+                    name='message'
+                    type='text'
+                    id='message'
+                    value={input.message}
+                    onChange={handleMessage}
+                />
+                <input
+                    className='chat--submit'
+                    type='submit'
+                    value='Send'
+                />
+            </form>
         </section>
     )
 }
